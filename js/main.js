6eb90694c0d2dfe055333b9fec23aa0b8acf0d24
humanMind <dmitrylarin786@gmail.com>
2022-01-26 17:09:58 +0300

    create: lesson-22 +  reviews and fix hotel slider

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
-const swiper = new Swiper('.swiper', {
+const hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button--next',
-    prevEl: '.swiper-button--prev',
+    nextEl: '.hotel-swiper__button--next',
+    prevEl: '.hotel-swiper__button--prev',
   },
+});
 
+const reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  },
 });
